63b0ccf81747be0cd6d9ae8b4a1803345cfaf105
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2024-10-31 12:09:10 -0700

    get and post added, working

--- app.py
+++ app.py
@@ -1,1 +1,46 @@
+from flask import Flask, request, jsonify, render_template
+from models import db, connect_db, Cupcake
+
 """Flask app for Cupcakes"""
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.app_context().push()
+
+connect_db(app)
+
+
+# **** API BELOW ***** 
+
+@app.route('/api/cupcakes')
+def get_all_cupcakes():
+    """returns JSON w/ all cupcakes"""
+    
+    all_cupcakes = [cc.serialize() for cc in Cupcake.query.all()] 
+    return jsonify(cupcakes=all_cupcakes)
+
+@app.route('/api/cupcakes/<int:cc_id>')
+def get_cupcake(cc_id):
+    '''returns JSON for 1 cupcake'''
+
+    cc = Cupcake.query.get_or_404(cc_id)
+    return jsonify(cupcake=cc.serialize())
+
+@app.route('/api/cupcakes', methods=['POST'])
+def post_cupcake():
+    '''creates cupcake and returns JSON'''
+
+    new_cc = Cupcake(flavor = request.json['flavor'], 
+                     size = request.json['size'],
+                     rating = request.json['rating'],
+                     image = request.json.get('image')
+                     )
+    db.session.add(new_cc)
+    db.session.commit()
+    response = jsonify(cupcake=new_cc.serialize())
+    return (response, 201)
+
+
--- models.py
+++ models.py
@@ -1,1 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() 
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
 """Models for Cupcake app."""
+
+class Cupcake(db.Model):
+    """a model to make cupcakes"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String, nullable=False)
+    size = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String, default='https://tinyurl.com/demo-cupcake')
+
+    def serialize(self):
+        '''returns dict of cupcake info'''
+
+        return {
+            'id': self.id,
+            'flavor': self.flavor,
+            'size' : self.size,
+            'rating': self.rating,
+            'image' : self.image
+        }
+
+
